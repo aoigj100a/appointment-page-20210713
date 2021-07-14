74b96f691a3a0f7a24c7f3759bd6ad04fbc1b1e4
aoigj100a <aoigj100@gmail.com>
2021-07-14 16:33:59 +0800

    add type stupid way XDDD

--- js/script.js
+++ js/script.js
@@ -33,37 +33,116 @@ $(items[11]).attr("deta-name", "退回")
 //   }
 // }
 
-class TypeWriter {
-  constructor() {
-    this.board = $("#board"),
-    this.boardContent = $("#board").html(),
-    this.action = {
+// class TypeWriter {
+//   constructor() {
+//     this.board = $("#board"),
+//     this.boardContent = $("#board").html(),
+//     this.action = {
 
-      getVal: n => $(items[n]).attr("deta-name"),
-      tpyeToBoard: (Val) => {
-          let newContent = this.board.html(Val)
-          return newContent
-        },
-      backNextText: () => {
-          let newTextArr = this.boardContent.split('')
-          let removedContentArr = newTextArr.pop()
-          let newBoardContent = newTextArr.join('')
-          return newBoardContent
-        },
-        clearBoard: ()=>{
-        let newTextArr = this.boardContent.html("")
-        return newTextArr
-        },
-      }
-  }
-  init(){
-    const self = this
-  }
-}
+//       getVal: n => $(items[n]).attr("deta-name"),
+//       tpyeToBoard: (Val) => {
+//           let newContent = this.board.html(Val)
+//           return newContent
+//         },
+//       backNextText: () => {
+//           let newTextArr = this.boardContent.split('')
+//           let removedContentArr = newTextArr.pop()
+//           let newBoardContent = newTextArr.join('')
+//           return newBoardContent
+//         },
+//         clearBoard: ()=>{
+//         let newTextArr = this.boardContent.html("")
+//         return newTextArr
+//         },
+//       }
+//   }
+//   init(){
+//     const self = this
+//   }
+// }
 
-items[0].click(()=>{
+let board = $("#board")
+let temp =[]
+items[0].click(() => {
+  temp.push($(items[0]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
 
-  console.log(TypeWriter)
-  // alert('ok')
+items[1].click(() => {
+  temp.push($(items[1]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
 
+items[2].click(() => {
+  temp.push($(items[2]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[3].click(() => {
+  temp.push($(items[3]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[4].click(() => {
+  temp.push($(items[4]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[5].click(() => {
+  temp.push($(items[5]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[6].click(() => {
+  temp.push($(items[6]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[7].click(() => {
+  temp.push($(items[7]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[8].click(() => {
+  temp.push($(items[8]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[9].click(() => {
+  temp = []
+  board.html('請用畫面鍵盤輸入電話號碼')
+  console.log(temp)
+  return temp
+})
+
+items[10].click(() => {
+  temp.push($(items[10]).attr("deta-name")) 
+  board.html(temp)
+  console.log(temp)
+  return temp
+})
+
+items[11].click(() => {
+  temp.pop() 
+  board.html(temp)
+  console.log(temp)
+  return temp
 })
