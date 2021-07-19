db2b8d34683caccc82a88334ebe20c84563065f9
aoigj100a <aoigj100@gmail.com>
2021-07-19 10:17:06 +0800

    feat:Board limit 10

--- js/script.js
+++ js/script.js
@@ -6,33 +6,38 @@ let temp = []
 getItems.forEach((item, index) => {
   item.setAttribute('id', index)
   if (index === 9) {
-        item.addEventListener('click', () => {
-          clearBoard()
-        })
-      } else if (index === 11) {
-        item.addEventListener('click', () => {
-          backNextText()
-        })
-      } else {
-        item.addEventListener('click', () => {
-          typeToBoard(item)
-        })
-      }
-  })
-  confirmBtn.addEventListener("click",()=>{
-    if (confirmPhone(board.innerText)) {
-      alert('ok')
-    }else{
-      alert('不ok')
+    item.addEventListener('click', () => {
       clearBoard()
-    }
-  })
+    })
+  } else if (index === 11) {
+    item.addEventListener('click', () => {
+      backNextText()
+    })
+  } else {
+    item.addEventListener('click', () => {
+      typeToBoard(item)
+    })
+  }
+})
+confirmBtn.addEventListener("click", () => {
+  if (confirmPhone(board.innerText)) {
+    alert('手機認證通過')
+  } else {
+    alert(`${board.innerText}不是一個合法的電話號碼！`)
+    clearBoard()
+  }
+})
 
 function typeToBoard(item) {
-  temp.push(item.dataset.name)
-  board.innerText = temp.join("")
-  board.classList.add("typing")
-  return temp.slice("")
+  if (temp.length <= 10) {
+    temp.push(item.dataset.name)
+    board.innerText = temp.join("")
+    board.classList.add("typing")
+    return temp.slice("")
+  } else {
+    alert('不能超過10個碼喔！')
+  }
+
 }
 function backNextText() {
   if (temp.length <= 1 || temp === "請用畫面鍵盤輸入電話號碼") {
